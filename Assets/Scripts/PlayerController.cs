98bde915c3fed3fb108bf9bdab8e5279475c330a
Alex Kharouk <akharoukpp@gmail.com>
2019-11-10 19:19:53 +0000

    Fix set up

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,8 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Hello World!
+        Debug.Log("Sup dude?");
     }
 
     // Update is called once per frame
