a3ab6473e0ead94660acb2f6527118b7005d7cdd
Alex Kharouk <akharoukpp@gmail.com>
2019-11-11 06:57:31 +0000

    Convert to Mac Computer

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,10 +14,10 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-
     void Update() // 60 FPS
     {
         // transform.Translate(0, 0, 1); <- Un-needed, We got better ways of handling motion.
         transform.Translate(speed * Time.deltaTime * Vector3.forward); // does the same thing ^.
     }
 }
+
